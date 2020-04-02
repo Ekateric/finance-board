4ffb70b51ce91faa6b8ab63545c6b0bb2354a65d
Ekaterina Kireeva <e.radakina@gmail.com>
2020-04-02 08:10:13 +0300

    Add Total & Categories components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,32 @@
 import React, { Component } from 'react';
 import Total from './components/Total/Total';
+import CategoriesList from './components/Categories/List';
 
 class App extends Component {
   render() {
+    const categories = [
+      {
+        title: 'Common',
+        moneySum: 1200,
+      },
+      {
+        title: 'Child',
+        moneySum: 300,
+      },
+      {
+        title: 'My own',
+        moneySum: 120,
+      },
+      {
+        title: 'Flat',
+        moneySum: 200,
+      }
+    ];
+    
     return (
       <div className="finance-board">
         <Total />
+        <CategoriesList categoriesData={categories} />
       </div>
     )
   }
